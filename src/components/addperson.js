6ccc14e89bf53e8518d98230bb632e6f6a7427b1
mir <mirnumbing@gmail.com>
2018-03-28 20:54:40 +0200

    name can be add

--- src/components/addperson.js
+++ src/components/addperson.js
@@ -2,21 +2,46 @@ import React, {Component} from 'react';
 import Table from './table';
 
 class Add extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            person:[]
+        };
+    }
+    add(){
+        let name = this.refs.name.value;
+        console.log(name);
+        if(localStorage.getItem('person')== null){
+            let person = [];
+            person.push(name);
+            localStorage.setItem('person',JSON.stringify(person));
+        }else{
+            let person = JSON.parse(localStorage.getItem('person'));
+            person.push(name);
+            localStorage.setItem('person', JSON.stringify(person));
+        }
+        this.setState({
+            person: JSON.parse(localStorage.getItem('person'))
+        });
+    }
+
+
     render() {
         return (
             <div className="add">
                 <h2>Add new person</h2>
                 <form className="form-inline">
                     <div className="form-group">
-                        <input type="text" name="name" placeholder="Name"/>
+                        <input type="text" ref="name" name="name" placeholder="Name"/>
                         <p><input type="checkbox" className="checkbox-inline" name="super-power"/> Super power</p>
                         <p><input type="checkbox" className="checkbox-inline" name="rich"/> rich</p>
                         <p><input type="checkbox" className="checkbox-inline" name="genius"/> Genius</p>
-                        <button type="button" className="btn btn-primary">Add</button>
+                        <button type="button" onClick={this.add.bind(this)} className="btn btn-primary">Add</button>
                     </div>
                 </form>
 
-                <Table/>
+                <Table person={this.state.person}/>
             </div>
         );
     }
--- src/components/table.js
+++ src/components/table.js
@@ -1,29 +1,36 @@
 import React, {Component} from 'react';
 
 class Table extends Component {
+
     render() {
         return (
             <div className="data-view">
                 <table className="table table-bordered table-striped">
 
                     <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Super Power</th>
-                            <th>Rich</th>
-                            <th>Genius</th>
-                            <th>Delete</th>
-                        </tr>
+                    <tr>
+                        <th>Name</th>
+                        <th>Super Power</th>
+                        <th>Rich</th>
+                        <th>Genius</th>
+                        <th>Delete</th>
+                    </tr>
                     </thead>
+                    <tbody>
+                    {this.props.person.map(function (name) {
+                        return (<tr>{name}</tr>);
+                    })}
+                    </tbody>
+
 
                     <tbody>
-                        <tr>
-                            <td>name will added</td>
-                            <td><input type='checkbox'/></td>
-                            <td><input type='checkbox'/></td>
-                            <td><input type='checkbox'/></td>
-                            <td>X</td>
-                        </tr>
+                    <tr>
+                        <td>name will added</td>
+                        <td><input type='checkbox'/></td>
+                        <td><input type='checkbox'/></td>
+                        <td><input type='checkbox'/></td>
+                        <td>X</td>
+                    </tr>
 
 
                     </tbody>
